63cd97dfb53dc8a9ca2d7cb92263814af685702d
Ian Davis <jungziege@gmail.com>
2022-03-08 12:58:21 -0800

    Update index copy

--- pages/index.jsx
+++ pages/index.jsx
@@ -40,28 +40,45 @@ export default function ExquisiteIntro() {
             Click the button below to get started. You’ll be taken to a screen
             with one sentence, written by the previous participant. Add on to
             the story by submitting a new piece of text that furthers the
-            narrative.
-          </p>
-          <p>
-            After you’ve submitted your next piece of writing, you’ll be able to
-            view the entirety of the story up from all other participants up
-            until the point you submitted.
+            narrative. After you’ve submitted your next piece of writing, you’ll be able to
+            view the entirety of the story from all other participants.
           </p>
           <h2>Who built this?</h2>
           <p>
-            Mysilio is a consulting firm dedicated to building web technology
-            that supports networks of caring, interdependent humans. This
-            experience was built using Solid [link], and through the generous
-            grant funding of Grant for the Web [link].
+            <a href="https://mysilio.com/">Mysilio</a> is a consulting firm
+            dedicated to building web technology that supports networks of
+            caring, interdependent humans. This is one of many experiments we're
+            running with Web Monetization and Solid, supported by the generous
+            grant funding of&nbsp;
+            <a href="https://www.grantfortheweb.org/">Grant for the Web</a>. To
+            learn more about our work, our experiments, or to particpate in the
+            beta of our new Web Monetized publishing platform,&nbsp;
+            <a
+              className="font-bold"
+              href="https://mailchi.mp/865966fe3848/mysilio-waitlist"
+            >
+              join our mailing list.
+            </a>
+          </p>
+          <h2>What is Solid?</h2>
+          <p>
+            <a href="https://solidproject.org/">Social Linked Data</a>, or
+            Solid, is a proposed W3C standard for hosting personal data in
+            shared RDF datastores than can be used by any application. Solid
+            provides a rich toolset for data interoperability and storage,
+            allowing interactive applications like this one to be built quickly
+            without deploying additional backend servers.
           </p>
           <h2>What is Web Monetization?</h2>
           <p>
-            Web Monetization is a proposed API standard that enables website
-            visitors with the ability to stream micropayments to content
-            creators&nbsp;
             <a href="https://webmonetization.org/docs/explainer/">
-              https://webmonetization.org/docs/explainer/
+              Web Monetization
             </a>
+            &nbsp; is a proposed W3C standard that enables website visitors with
+            the ability to stream micropayments directly to content creators,
+            tip to reward particularly good content, and even gate content on
+            payment, providing a viable alternative to advertising based
+            buisiness models on the web.
           </p>
           {!isMonetizing ? (
             <h2>Please enable Web Monetization to participate</h2>
