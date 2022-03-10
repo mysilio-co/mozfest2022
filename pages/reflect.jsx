282845ac7e2780b7147a8c45c7258f22d43f1247
Ian Davis <jungziege@gmail.com>
2022-03-10 15:18:50 -0800

    Only let folks submit once

--- pages/reflect.jsx
+++ pages/reflect.jsx
@@ -2,12 +2,15 @@ import Head from "next/head";
 import { useReflections} from "../model/story";
 import { AddToStory, DisplayStory } from "../components/Story"
 import { MysilioPointer } from "../components/MonetizationPicker";
+import { useLocalStorage } from "../model/utils";
 
 export default function ExquisiteStory() {
   const { resource, save } = useReflections();
+  const [submitted, setSubmitted] = useLocalStorage("submitted", false);
 
   async function saveAndDisplayStory(newStory) {
     await save(newStory);
+    setSubmitted(true);
   }
 
   return (
@@ -18,12 +21,18 @@ export default function ExquisiteStory() {
       <section className="min-h-screen mx-auto w-4/5 bg-white/90 shadow-2xl py-20">
         <div className="relative px-4 sm:px-6 lg:px-8">
           <div className="text-lg max-w-prose mx-auto">
-            {resource && (
-              <>
-                <AddToStory story={resource} saveStory={saveAndDisplayStory} />
+            {resource &&
+              (submitted ? (
                 <DisplayStory story={resource} />
-              </>
-            )}
+              ) : (
+                <>
+                  <AddToStory
+                    story={resource}
+                    saveStory={saveAndDisplayStory}
+                  />
+                  <DisplayStory story={resource} />
+                </>
+              ))}
           </div>
         </div>
       </section>
